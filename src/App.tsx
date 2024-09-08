d5a7e3a31e9901e9e2766558f65d05f1902e2b83
sandeepatel01 <sandeepatel.0101@gmail.com>
2024-09-08 22:05:01 +0530

    Add Routing

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,29 @@
+import { Routes, Route } from "react-router-dom";
+import SigninForm from "./_auth/forms/SigninForm";
+import SignupForm from "./_auth/forms/SignupForm";
+import AuthLayout from "./_auth/AuthLayout";
+
+import { Home } from "./_root/pages/index";
+import RootLayout from "./_root/RootLayout";
 import "./globals.css";
 
 const App = () => {
-  return <h1 className="text-3xl font-bold underline">Hello ErrorBook!</h1>;
+  return (
+    <main>
+      <Routes>
+        {/* Public Routes  */}
+        <Route element={<AuthLayout />}>
+          <Route path="/sign-in" element={<SigninForm />} />
+          <Route path="/sign-up" element={<SignupForm />} />
+        </Route>
+
+        {/* Private Routes  */}
+        <Route element={<RootLayout />}>
+          <Route index element={<Home />} />
+        </Route>
+      </Routes>
+    </main>
+  );
 };
 
 export default App;
