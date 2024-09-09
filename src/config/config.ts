fcf0e8943d3d2d9836d84eb74a1cf016e6f01a2d
sandeepatel01 <sandeepatel.0101@gmail.com>
2024-09-09 21:48:33 +0530

    Add appwrite Storage & Database config

--- src/config/config.ts
+++ src/config/config.ts
@@ -1,6 +1,17 @@
 const config = {
   appwriteUrl: String(import.meta.env.VITE_APPWRITE_URL),
   appwriteProjectId: String(import.meta.env.VITE_APPWRITE_PROJECT_ID),
+  appwriteBucketId: String(import.meta.env.VITE_APPWRITE_BUCKET_ID),
+  appwriteDatabaseId: String(import.meta.env.VITE_APPWRITE_DATABASE_ID),
+  appwriteSavesCollectionId: String(
+    import.meta.env.VITE_APPWRITE_SAVES_COLLECTION_ID
+  ),
+  appwriteUsersCollectionId: String(
+    import.meta.env.VITE_APPWRITE_USERS_COLLECTION_ID
+  ),
+  appwriteErrorPostsCollectionId: String(
+    import.meta.env.VITE_APPWRITE_ERRORPOSTS_COLLECTION_ID
+  ),
 };
 
 export default config;
