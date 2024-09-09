761203a419307f28052000708971ec61fe243164
sandeepatel01 <sandeepatel.0101@gmail.com>
2024-09-09 22:25:54 +0530

    Add newUser create & save TODB functionality

--- src/lib/appwrite/auth.api.ts
+++ src/lib/appwrite/auth.api.ts
@@ -1,16 +1,20 @@
-import { Client, Account, ID } from "appwrite";
+import { Client, Account, ID, Avatars, Databases } from "appwrite";
 import config from "../../config/config";
 import { INewUser } from "@/types";
 
-class Service {
+export class Service {
   client = new Client();
   account;
+  avatars;
+  databases;
 
   constructor() {
     this.client
       .setEndpoint(config.appwriteUrl)
       .setProject(config.appwriteProjectId);
     this.account = new Account(this.client);
+    this.avatars = new Avatars(this.client);
+    this.databases = new Databases(this.client);
   }
 
   // ============================== SIGN UP
@@ -22,10 +26,44 @@ class Service {
         user.name,
         user.password
       );
-      return newAccount;
+
+      if (!newAccount) throw Error;
+
+      const avatarUrl = this.avatars.getInitials(user.name);
+
+      const newUser = await this.saveUserToDB({
+        accountId: newAccount.$id,
+        name: newAccount.name,
+        email: newAccount.email,
+        username: user.username,
+        imageUrl: avatarUrl,
+      });
+
+      return newUser;
     } catch (error) {
       console.log("Error: ", error);
       return error;
+    }
+  }
+
+  // ============================== SAVE USER TO DB
+  async saveUserToDB(user: {
+    accountId: string;
+    email: string;
+    name: string;
+    imageUrl: URL;
+    username?: string;
+  }) {
+    try {
+      const newUser = await this.databases.createDocument(
+        config.appwriteDatabaseId,
+        config.appwriteUsersCollectionId,
+        ID.unique(),
+        user
+      );
+      return newUser;
+    } catch (error) {
+      console.log(error);
     }
   }
 }
