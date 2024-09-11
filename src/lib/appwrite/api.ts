43b99bee1990685ff171bc49f0d33dd838ef578a
sandeepatel01 <sandeepatel.0101@gmail.com>
2024-09-11 20:18:41 +0530

    Add Topbar, signOutAccount

--- src/lib/appwrite/api.ts
+++ src/lib/appwrite/api.ts
@@ -106,3 +106,13 @@ export async function getCurrentUser() {
     throw new Error("Error getting current user");
   }
 }
+
+export async function signOutAccount() {
+  try {
+    const session = await account.deleteSession("current");
+
+    return session;
+  } catch (error) {
+    console.log(error);
+  }
+}
