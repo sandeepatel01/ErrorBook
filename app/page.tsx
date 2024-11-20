510733c4f309b3ed0bf3db440eb80975f7ce8523
sandeepatel01 <sandeepatel.0101@gmail.com>
2024-11-20 22:11:36 +0530

    Add Authentication with clerk

--- app/page.tsx
+++
@@ -1,9 +0,0 @@
-export default function Home() {
-  return (
-    <div>
-      <h1 className="h1-bold flex">
-        ErrorBook - The web book of errors for the Developer{" "}
-      </h1>
-    </div>
-  );
-}
