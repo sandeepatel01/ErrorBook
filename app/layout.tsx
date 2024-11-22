4b8771823220b96ce632d3f99c596cce2acb0cf0
sandeepatel01 <sandeepatel.0101@gmail.com>
2024-11-22 18:45:09 +0530

    Add layout of project

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,11 +1,27 @@
-import {
-  ClerkProvider,
-  SignInButton,
-  SignedIn,
-  SignedOut,
-  UserButton,
-} from "@clerk/nextjs";
+import { ClerkProvider } from "@clerk/nextjs";
 import "./globals.css";
+import { Inter, Space_Grotesk } from "next/font/google";
+import type { Metadata } from "next";
+
+const inter = Inter({
+  subsets: ["latin"],
+  weight: ["100", "200", "300", "400", "500", "600", "700", "800", "900"],
+  variable: "--font-inter",
+});
+const spaceGrotesk = Space_Grotesk({
+  subsets: ["latin"],
+  weight: ["300", "400", "500", "600", "700"],
+  variable: "--font-spaceGrotesk",
+});
+
+export const metadata: Metadata = {
+  title: "Errorbook",
+  description:
+    "A community for developers to asking and answering questions, and collaborate with developers from around the world.",
+  icons: {
+    icon: "/assets/images/icon.jpg",
+  },
+};
 
 export default function RootLayout({
   children,
@@ -13,15 +29,16 @@ export default function RootLayout({
   children: React.ReactNode;
 }) {
   return (
-    <ClerkProvider>
+    <ClerkProvider
+      appearance={{
+        elements: {
+          formButtonPrimary: "primary-gradient",
+          footerActionLink: "primary-text-gradient hover:text-primary-500",
+        },
+      }}
+    >
       <html lang="en">
-        <body>
-          <SignedOut>
-            <SignInButton />
-          </SignedOut>
-          <SignedIn>
-            <UserButton />
-          </SignedIn>
+        <body className={`${inter.variable} ${spaceGrotesk.variable}`}>
           {children}
         </body>
       </html>
