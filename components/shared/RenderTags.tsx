a7bcd9b61d7acf02e3a3eb75e7155ac5e9973ecd
sandeepatel01 <sandeepatel.0101@gmail.com>
2024-11-25 21:45:05 +0530

    Add RightSIdebar component

---
+++ components/shared/RenderTags.tsx
@@ -0,0 +1,30 @@
+import React from "react";
+import { Badge } from "@/components/ui/badge";
+import Link from "next/link";
+
+interface RenderTagsProps {
+  _id: number;
+  name: string;
+  totalQuestions?: number;
+  showCount?: boolean;
+}
+
+const RenderTags = ({
+  _id,
+  name,
+  totalQuestions,
+  showCount,
+}: RenderTagsProps) => {
+  return (
+    <Link href={`/tags/${_id}`} className="flex justify-between gap-2">
+      <Badge className="subtle-medium background-light800_dark300 text-light400_light500 rounded-xl border-none px-4 py-2 uppercase">
+        {name}
+      </Badge>
+      {showCount && (
+        <p className="small-medium text-dark500_light700">{totalQuestions}</p>
+      )}
+    </Link>
+  );
+};
+
+export default RenderTags;
---
+++ components/shared/RightSidebar.tsx
@@ -0,0 +1,105 @@
+import Image from "next/image";
+import Link from "next/link";
+import { title } from "process";
+import React from "react";
+import RenderTags from "./RenderTags";
+
+const topQuestions = [
+  {
+    _id: 1,
+    title:
+      "How to Ensure Unique User Profile with ON CONFLICT in PostgreSQL Using Drizzle ORM?",
+  },
+  {
+    _id: 2,
+    title:
+      "What are the benefits and trade-offs of using Server-Side Rendering (SSR) in Next.js?",
+  },
+  {
+    _id: 3,
+    title: "How to center a div?",
+  },
+  {
+    _id: 4,
+    title:
+      "Node.js res.json() and res.send(), not working but still able to change status code",
+  },
+  {
+    _id: 5,
+    title: "ReactJs or NextJs for begginers i ask for advice",
+  },
+];
+
+const popularTags = [
+  {
+    _id: 1,
+    name: "react",
+    totalQuestions: 5,
+  },
+  {
+    _id: 2,
+    name: "javascript",
+    totalQuestions: 10,
+  },
+  {
+    _id: 3,
+    name: "html",
+    totalQuestions: 15,
+  },
+  {
+    _id: 4,
+    name: "css",
+    totalQuestions: 5,
+  },
+  {
+    _id: 5,
+    name: "python",
+    totalQuestions: 2,
+  },
+];
+
+const RightSidebar = () => {
+  return (
+    <section className="background-light900_dark200 light-border custom-scrollbar sticky right-0 top-0 flex h-screen w-[350px] flex-col overflow-y-auto border-l p-6 pt-36 shadow-light-300 dark:shadow-none max-xl:hidden">
+      <div>
+        <h3 className="h3-bold text-dark200_light900">Top Questions</h3>
+        <div className="mt-7 flex flex-col gap-[30px]">
+          {topQuestions.map((question) => (
+            <Link
+              href={`/questions/${question.id}`}
+              key={question.id}
+              className="flex cursor-pointer items-center justify-between gap-7"
+            >
+              <p className="body-medium text-dark500_light700">
+                {question.title}
+              </p>
+              <Image
+                src="/assets/icons/chevron-right.svg"
+                width={20}
+                height={20}
+                alt="chevron-right"
+                className="invert-colors"
+              />
+            </Link>
+          ))}
+        </div>
+      </div>
+      <div className="mt-16">
+        <h3 className="h3-bold text-dark200_light900">Popular Tags</h3>
+        <div className="mt-7 flex flex-col gap-4">
+          {popularTags.map((tag) => (
+            <RenderTags
+              key={tag._id}
+              _id={tag._id}
+              name={tag.name}
+              totalQuestions={tag.totalQuestions}
+              showCount
+            />
+          ))}
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default RightSidebar;
