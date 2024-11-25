68bcc45d998e3871d8fd9bc3e71e964c11972480
sandeepatel01 <sandeepatel.0101@gmail.com>
2024-11-25 20:24:44 +0530

    Add LeftSidebar component

--- components/shared/LeftSidebar.tsx
+++ components/shared/LeftSidebar.tsx
@@ -1,8 +1,82 @@
+"use client";
+
+import Link from "next/link";
 import React from "react";
+import { usePathname } from "next/navigation";
+import Image from "next/image";
+import { sidebarLinks } from "@/constants/constants";
+import { SignedOut } from "@clerk/nextjs";
+import { Button } from "../ui/button";
 
 const LeftSidebar = () => {
+  const pathname = usePathname();
+
   return (
-    <section className="light900_dark200 light-border sticky left-0 top-0 flex h-screen w-fit flex-col justify-between overflow-y-auto border-r p-6 pt-36 shadow-light-300 dark:shadow-none max-sm:hidden lg:w-[266px]"></section>
+    <section className="background-light900_dark200 light-border custom-scrollbar sticky left-0 top-0 flex h-screen flex-col justify-between overflow-y-auto border-r p-6 pt-36 shadow-light-300 dark:shadow-none max-sm:hidden lg:w-[266px]">
+      <div className="flex flex-1 flex-col gap-6">
+        {sidebarLinks.map((item) => {
+          const isActive =
+            (pathname.includes(item.route) && item.route.length > 1) ||
+            pathname === item.route;
+          return (
+            <Link
+              key={item.route}
+              href={item.route}
+              className={`${
+                isActive
+                  ? "primary-gradient rounded-xl text-light-900"
+                  : "text-dark300_light900"
+              } flex items-center justify-start gap-4 bg-transparent p-4`}
+            >
+              <Image
+                src={item.imgURL}
+                width={20}
+                height={20}
+                alt={item.label}
+                className={`${isActive ? "" : "invert-colors"}`}
+              />
+              <p
+                className={`${isActive ? "base-bold" : "base-medium"} max-lg:hidden`}
+              >
+                {item.label}
+              </p>
+            </Link>
+          );
+        })}
+      </div>
+
+      <SignedOut>
+        <div className="flex flex-col gap-3">
+          <Link href="/sign-in">
+            <Button className="small-medium btn-secondary min-h-[41px] w-full rounded-xl px-4 py-3 shadow-none">
+              <Image
+                src="/assets/icons/account.svg"
+                width={20}
+                height={20}
+                alt="login"
+                className="invert-colors lg:hidden"
+              />
+              <span className="primary-text-gradient max-lg:hidden">
+                Log In
+              </span>
+            </Button>
+          </Link>
+
+          <Link href="/sign-up">
+            <Button className="small-medium light-border-2 btn-tertiary text-dark400_light900 min-h-[41px] w-full rounded-xl px-4 py-3 shadow">
+              <Image
+                src="/assets/icons/sign-up.svg"
+                width={20}
+                height={20}
+                alt="sign up"
+                className="invert-colors lg:hidden"
+              />
+              <span className="max-lg:hidden">Sign up</span>
+            </Button>
+          </Link>
+        </div>
+      </SignedOut>
+    </section>
   );
 };
 
