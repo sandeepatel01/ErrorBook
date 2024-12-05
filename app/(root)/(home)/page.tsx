23c9d681834a9f3b2e39941d6463736fc3206a64
sandeepatel01 <sandeepatel.0101@gmail.com>
2024-12-05 23:30:16 +0530

    Clean Code

--- app/(root)/(home)/page.tsx
+++ app/(root)/(home)/page.tsx
@@ -67,27 +67,3 @@ export default async function Home() {
     </>
   );
 }
-
-// "use client";
-
-// import { UserButton } from "@clerk/nextjs";
-// const DotIcon = () => {
-//   return (
-//     <svg
-//       xmlns="http://www.w3.org/2000/svg"
-//       viewBox="0 0 512 512"
-//       fill="currentColor"
-//     >
-//       <path d="M256 512A256 256 0 1 0 256 0a256 256 0 1 0 0 512z" />
-//     </svg>
-//   );
-// };
-
-// const CustomPage = () => {
-//   return (
-//     <div>
-//       <h1>Custom Profile Page</h1>
-//       <p>This is the custom profile page</p>
-//     </div>
-//   );
-// };
