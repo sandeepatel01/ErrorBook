5fa8e20fc6429c8380e6510fa94075ff4babe947
sandeepatel01 <sandeepatel.0101@gmail.com>
2024-12-10 11:15:25 +0530

    Implement server action for answer

---
+++ lib/actions/answer.action.ts
@@ -0,0 +1,33 @@
+"use server";
+
+import Answer from "@/models/answer.model";
+import { CreateAnswerParams } from "./shared.types";
+import { connectToDatabase } from "../dbConnect";
+import Question from "@/models/question.model";
+import { revalidatePath } from "next/cache";
+
+export async function createAnswer(params: CreateAnswerParams) {
+  await connectToDatabase();
+
+  try {
+    const { content, author, question, path } = params;
+
+    const newAnswer = new Answer({
+      content,
+      author,
+      question,
+    });
+
+    // Add the answer to the question's answers array
+    await Question.findByIdAndUpdate(question, {
+      $push: { answers: newAnswer._id },
+    });
+
+    // TODO: Create an interaction record for the user's answer action
+
+    revalidatePath(path);
+  } catch (error) {
+    console.log("Error creating answer: ", error);
+    throw error;
+  }
+}
