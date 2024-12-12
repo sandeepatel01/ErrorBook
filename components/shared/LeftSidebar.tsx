da5c4fef8f08bce719cb8c23d2922bc90c47af3f
sandeepatel01 <sandeepatel.0101@gmail.com>
2024-12-12 15:43:19 +0530

    fix: handle undefined userId and prevent dynamic href error

--- components/shared/LeftSidebar.tsx
+++ components/shared/LeftSidebar.tsx
@@ -5,11 +5,18 @@ import React from "react";
 import { usePathname } from "next/navigation";
 import Image from "next/image";
 import { sidebarLinks } from "@/constants/constants";
-import { SignedOut } from "@clerk/nextjs";
+import { SignedOut, useAuth } from "@clerk/nextjs";
 import { Button } from "../ui/button";
 
 const LeftSidebar = () => {
+  const { isLoaded, userId } = useAuth();
+  console.log("User Id: ", userId);
   const pathname = usePathname();
+
+  if (!isLoaded) {
+    // Prevent rendering until the auth state is fully loaded
+    return <div>Loading...</div>;
+  }
 
   return (
     <section className="background-light900_dark200 light-border custom-scrollbar sticky left-0 top-0 flex h-screen flex-col justify-between overflow-y-auto border-r p-6 pt-36 shadow-light-300 dark:shadow-none max-sm:hidden lg:w-[266px]">
@@ -18,10 +25,28 @@ const LeftSidebar = () => {
           const isActive =
             (pathname.includes(item.route) && item.route.length > 1) ||
             pathname === item.route;
+
+          // if (item.route === "/profile") {
+          //   if (userId) {
+          //     item.route = `${item.route}/${userId}`;
+          //   } else {
+          //     return null;
+          //   }
+          // }
+
+          const dynamicRoute =
+            item.route === "/profile" && userId
+              ? `${item.route}/${userId}`
+              : item.route;
+
+          if (item.route === "/profile" && !userId) {
+            return null;
+          }
+
           return (
             <Link
-              key={item.route}
-              href={item.route}
+              key={dynamicRoute}
+              href={dynamicRoute}
               className={`${
                 isActive
                   ? "primary-gradient rounded-xl text-light-900"
